96555d77af116e63ad133cf35e586baeee13cb4c
Gowri Kumar Chandramouli <gchandramouli@versium.com>
2019-03-20 15:38:10 -0700

    api calls to b2bemail

--- monitoring/TestRunner.php
+++ monitoring/TestRunner.php
@@ -6,32 +6,32 @@
  * Time: 12:53 PM
  */
 
-define("AVG_TIME_PER_RECORD","average_time_per_record_in_milliseconds");
+define("ERROR", "error");
+define("RESULT", "result");
 
-define("BUSINESS_INFO","/businessinfo");
 
-define("END_TIME","execution_end_time");
-define("ERROR","error");
-define("ERRORS",'errors');
+define("PRODID", "prodids");
 
-define("FAILED_COUNT","failed_count");
-define("FAILED_RECORDS","failed_records");
-define("KEY",'key');
 
-define("NO_RESPONSE_COUNT","no_response_count");
-define("NO_RESPONSE_RECORDS","no_response_records");
+define("B2B_EMAIL_PRODID", "b2bemail");
+define("EMAIL_PRODID", "email");
 
-define("PASSED_COUNT","passed_count");
-define("PASSED_RECORDS","passed_records");
-define("RESULT","result");
 
-define("START_TIME","execution_start_time");
+define("KEY", 'vkey');
+define("CFG_OUTPUT", "cfg_output");
+define("OUTPUT_STATS2", "stats2");
 
-define("TEST_RECORDS",'test_records');
-define("TICKER",'ticker');
-define("TIME_TAKEN_IN_MILLI_SECONDS","time_taken_in_milliseconds");
-define("TOTAL_RECORDS_CHECKED",'number_of_records');
-
+define("FIRST_NAME","d_first");
+define("LAST_NAME", "d_last");
+define("PHONE", "d_phone");
+define("CITY", "d_city");
+define("STATE", "d_state");
+define("ZIP", "d_zip");
+define("BUSINESS_NAME", "d_busname");
+define("EMAIL", "d_email");
+define("ADDRESS", "d_fulladdr");
+define("TICKER", "d_ticker");
+define("DOMAIN", "d_domain");
 
 
 class TestRunner
@@ -68,7 +68,7 @@ class TestRunner
     function CallAPI($method, &$url, $data = false)
     {
 
-        $response =[];
+        $response = [];
 
         $curl = curl_init();
 
@@ -97,24 +97,22 @@ class TestRunner
 
         $result = curl_exec($curl);
 
-        if($result === false)
-        {
+        if ($result === false) {
             $response[ERROR] = "CURL Error: " . curl_error($curl);
 
         }
 
-        /*
+
         $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
         if ($responseCode >= 400) {
             $response[ERROR] = "HTTP Error: " . $responseCode;
         }
-        */
+
 
         curl_close($curl);
 
-
         //print_r($response[ERROR]);
-        $response[RESULT]=json_decode($result, true, 10);
+        $response[RESULT] = json_decode($result, true, 10);
         //print_r($result);
 
         return $response;
@@ -127,143 +125,142 @@ class TestRunner
 
     }
 
-    protected function getParams($record)
+    protected function getParams($record, $inputParamCombination)
     {
         $params = [
-            KEY => '96408630-a6c8-4530-9060-fd8fe576979c',
-            //KEY => '96408630-a6c8-4530-9060-fd8fe576979',
-            TICKER => $record['Ticker']
+            KEY => '3b5ac175f8938aec76944acdc8314ec9'
         ];
+
+        foreach ($inputParamCombination as $param) {
+            $params[$param] = $record[$param];
+        }
 
         return $params;
     }
 
 
-    public function runTests($url, $api,$inputFileName,&$report)
+    public function runTests($url, $prodid, $cfg_output, $inputParamCombinations, $inputFileName)
     {
         $data = $this->csv_to_array($inputFileName);
-        $report[TOTAL_RECORDS_CHECKED] = count($data);
 
+        $csvFileName = 'example.csv';
+        $fp = fopen($csvFileName, 'w');
 
+        $test['url'] = '';
+        $test['num-results'] = '';
+        fputcsv($fp, array_keys($test));
 
         foreach ($data as $record) {
-            $params = $this->getParams($record);
+            foreach ($inputParamCombinations as $inputParamCombination) {
+                print_r($record);
+                $tempURL = $url;
 
-            //print_r($params);
+                $params = $this->getParams($record, $inputParamCombination);
+                $params[PRODID] = $prodid;
+                $params[CFG_OUTPUT] = $cfg_output;
 
-            $test = [];
-
-
-            $test[START_TIME] = microtime(true);
-
-            $tempurl = $url.$api;
-            $response = $this->runTest($tempurl, $params);
-
-
-            $test[END_TIME] = microtime(true);
-
-            $test[TIME_TAKEN_IN_MILLI_SECONDS] = round(($test[END_TIME] - $test[START_TIME]),3);
-            $report[AVG_TIME_PER_RECORD] = $report[AVG_TIME_PER_RECORD] + $test[TIME_TAKEN_IN_MILLI_SECONDS];
-            $test['url'] = urldecode($tempurl);
-
-            echo "Fetching data for : ";
-            print_r($test['url']);
-            echo "\n";
-
-            if(empty($response[ERROR])) {
-                //we got a response back from server
-
-                $test['result'] = $response[RESULT];
+                print_r($params);
+                $response = $this->runTest($tempURL, $params);
 
                 //print_r($response);
-                //print_r($result);
-                $num_results = $response[RESULT]['versium']['num_results'];
+
+                $test['url'] = urldecode($tempURL);
+                $test['num-results'] = $response[RESULT]['Versium']['num-results'];
+                //$test['result'] = $response[RESULT];
+
+                print_r($test);
+                fputcsv($fp, $test);
+
+
+                $num_results = $response[RESULT]['Versium']['num-results'];
 
                 //print_r($num_results);
-                if ($num_results == 1) {
-                    if (strtoupper($record['BusinessName']) === $response[RESULT]['versium']['results'][0]['business']) {
 
-                        array_push($report[TEST_RECORDS][PASSED_RECORDS], $test);
-                    } else {
 
-                        $test['FailureReason'] = "Data Mismatch";
-                        array_push($report[TEST_RECORDS][FAILED_RECORDS], $test);
-                    }
-                } elseif ($num_results == 0) {
+                //break;
+                /*
+                            $tempurl = $url;
+                            $response = $this->runTest($tempurl, $params);
 
-                    if (array_key_exists(ERRORS, $response[RESULT]['versium'])) {
-                        $test['FailureReason'] = "Error";
+                            $test['url'] = urldecode($tempurl);
 
-                    } else {
-                        $test['FailureReason'] = "No Data";
-                    }
-                    array_push($report[TEST_RECORDS][FAILED_RECORDS], $test);
+                            echo "Fetching data for : ";
+                            print_r($test['url']);
+                            echo "\n";
 
-                } else {
+                            if(empty($response[ERROR])) {
+                                //we got a response back from server
+                                $test['result'] = $response[RESULT];
 
-                    $test['FailureReason'] = "Multiple Records";
-                    array_push($report[TEST_RECORDS][FAILED_RECORDS], $test);
+                                print_r($response);
+                                print_r($result);
+                                $num_results = $response[RESULT]['versium']['num_results'];
 
-                }
+                                //print_r($num_results);
+                                if ($num_results == 1) {
+                                    if (strtoupper($record['BusinessName']) === $response[RESULT]['versium']['results'][0]['business']) {
+
+                                        array_push($report[TEST_RECORDS][PASSED_RECORDS], $test);
+                                    } else {
+
+                                        $test['FailureReason'] = "Data Mismatch";
+                                        array_push($report[TEST_RECORDS][FAILED_RECORDS], $test);
+                                    }
+                                } elseif ($num_results == 0) {
+
+                                    if (array_key_exists(ERRORS, $response[RESULT]['versium'])) {
+                                        $test['FailureReason'] = "Error";
+
+                                    } else {
+                                        $test['FailureReason'] = "No Data";
+                                    }
+                                    array_push($report[TEST_RECORDS][FAILED_RECORDS], $test);
+
+                                } else {
+
+                                    $test['FailureReason'] = "Multiple Records";
+                                    array_push($report[TEST_RECORDS][FAILED_RECORDS], $test);
+
+                                }
+                            }
+                            else
+                            {
+                                //curl error has occurred
+                                $test['FailureReason'] = $response[ERROR];
+                                //array_push($report[TEST_RECORDS][NO_RESPONSE_RECORDS], $test);
+                            }
+                            //break;
+                */
+
             }
-            else
-            {
-                //curl error has occurred
-                $test['FailureReason'] = $response[ERROR];
-                array_push($report[TEST_RECORDS][NO_RESPONSE_RECORDS], $test);
-            }
-
-
-
             //break;
-
         }
-
-
-
-        $report[PASSED_COUNT] = count($report[TEST_RECORDS][PASSED_RECORDS]);
-        $report[FAILED_COUNT] = count($report[TEST_RECORDS][FAILED_RECORDS]);
-        $report[NO_RESPONSE_COUNT] = count($report[TEST_RECORDS][NO_RESPONSE_RECORDS]);
-
-        $report[AVG_TIME_PER_RECORD]  = round($report[AVG_TIME_PER_RECORD] / count($data),3);
-
+        fclose($fp);
     }
+
 }
 
-$report = [];
-$report[START_TIME] = microtime(true);
 
-$report[FAILED_COUNT] = 0;
-$report[PASSED_COUNT] = 0;
-$report[NO_RESPONSE_COUNT] = 0;
-
-$report[AVG_TIME_PER_RECORD] = 0;
-$report[TEST_RECORDS] = [PASSED_RECORDS => [],FAILED_RECORDS=> [],NO_RESPONSE_RECORDS=>[]];
-
-
-//$main_url = "https://api.versium.com/v1.0";
-$main_url = "https://api.versium.com/v1.0";
-$api = BUSINESS_INFO;
-
-$fileName = "ticker_truth_set.csv";
+$main_url = "https://api2b.versium.com/q2.php";
+$fileName = "online-audience-b2c.csv";
+$prodIds = EMAIL_PRODID;
+$inputParamCombinations = [
+    [FIRST_NAME, LAST_NAME],
+    [FIRST_NAME, LAST_NAME, PHONE],
+    [FIRST_NAME, LAST_NAME, ADDRESS]
+];
+$cfg_output = OUTPUT_STATS2 . ',' . $prodIds;
 
 $testRunner = new TestRunner();
-
-$report['api'] = substr($api,1);
-
-$testRunner->runTests($main_url , $api, $fileName,$report);
-
-$report[END_TIME] = microtime(true);
-
-array_multisort($report,SORT_ASC);
-$json_output = json_encode($report,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-
-//print_r($json_output);
+# $testRunner->runTests($main_url, $prodIds, $cfg_output, $inputParamCombinations, $fileName);
 
 
+$fileName = "oracle.csv";
+$prodIds = B2B_EMAIL_PRODID;
+$inputParamCombinations = [
+    [FIRST_NAME, LAST_NAME, TICKER],
+    [FIRST_NAME, LAST_NAME, DOMAIN]
 
-//write json to file
-if (file_put_contents("output.json", $json_output))
-    echo "JSON file created successfully...\n";
-else
-    echo "Oops! Error creating json file...\n";
+];
+$cfg_output = OUTPUT_STATS2 . ',' . $prodIds;
+$testRunner->runTests($main_url, $prodIds, $cfg_output, $inputParamCombinations, $fileName);
